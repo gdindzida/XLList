4d8f57567bb08f8aadb19564ac75a544ced0b9b8
dzida <gdindzida@gmail.com>
2021-10-31 17:16:16 +0100

    Add tests

--- src/XLList.h
+++ src/XLList.h
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+
+// Declaration
+
 template<typename DataType>
 struct Node {
     DataType data;
@@ -36,8 +39,6 @@ public:
 
         Node<T> *getPreviousPointer() const;
 
-        Node<T>* xorNodes(Node<T> *first, Node<T> *second) const;
-
     public:
         Iterator() : nodePtrBefore(nullptr), nodePtrCurrent(nullptr) {};
 
@@ -54,6 +55,8 @@ public:
         Iterator operator--();
 
         Iterator operator--(int);
+
+        static Node<T>* xorNodes(Node<T> *first, Node<T> *second);
     };
 
     // Public methods
@@ -71,5 +74,217 @@ public:
     T popBack();
     int getSize();
 };
+
+// Definition
+
+// Private methods
+
+// Constructors and destructors
+template<typename T>
+XLList<T>::XLList() {
+    head = tail = new Node<T>();
+    size = 0;
+}
+
+template<typename T>
+XLList<T>::~XLList<T>() {
+    for (int iter = size-1; iter>=0; iter--){
+        removeElement(iter);
+    }
+    delete head;
+}
+
+// Iterator methods
+template<typename T>
+bool XLList<T>::Iterator::operator!=(const Iterator &comparingIterator) const {
+    return nodePtrBefore !=
+           comparingIterator.nodePtrBefore; // It doesn't matter which one you compare - before or current.
+}
+
+template<typename T>
+bool XLList<T>::Iterator::operator==(const Iterator &comparingIterator) const {
+    return (nodePtrBefore == comparingIterator.nodePtrBefore) && (nodePtrCurrent == comparingIterator.nodePtrCurrent);
+}
+
+template<typename T>
+T &XLList<T>::Iterator::operator*() const {
+    return nodePtrCurrent->data;
+}
+
+template<typename T>
+typename XLList<T>::Iterator XLList<T>::Iterator::operator++() {
+    Node<T> *tempNode = getNextPointer();
+    nodePtrBefore = nodePtrCurrent;
+    nodePtrCurrent = tempNode;
+    return *this;
+}
+
+template<typename T>
+typename XLList<T>::Iterator XLList<T>::Iterator::operator++(int) {
+    XLList<T>::Iterator temp = *this;
+    Node<T> *tempNode = getNextPointer();
+    nodePtrBefore = nodePtrCurrent;
+    nodePtrCurrent = tempNode;
+    return temp;
+}
+
+template<typename T>
+typename XLList<T>::Iterator XLList<T>::Iterator::operator--() {
+    Node<T> *tempNode = getPreviousPointer();
+    nodePtrCurrent = nodePtrBefore;
+    nodePtrBefore = tempNode;
+    return *this;
+}
+
+template<typename T>
+typename XLList<T>::Iterator XLList<T>::Iterator::operator--(int) {
+    XLList<T>::Iterator temp = *this;
+    Node<T> *tempNode = getPreviousPointer();
+    nodePtrCurrent = nodePtrBefore;
+    nodePtrBefore = tempNode;
+    return temp;
+}
+
+template<typename T>
+Node<T> *XLList<T>::Iterator::getNextPointer() const {
+    return xorNodes(nodePtrBefore, nodePtrCurrent->link);
+}
+
+template<typename T>
+Node<T> *XLList<T>::Iterator::getPreviousPointer() const {
+    return xorNodes(nodePtrBefore->link, nodePtrCurrent);
+}
+
+template<typename T>
+Node<T> *XLList<T>::Iterator::xorNodes(Node<T> *first, Node<T> *second) {
+    return reinterpret_cast<Node<T> *>(reinterpret_cast<uintptr_t>(first) ^ reinterpret_cast<uintptr_t>(second));
+}
+
+// Public methods
+template<typename T>
+typename XLList<T>::Iterator XLList<T>::begin() const {
+    return Iterator(head, head->link);
+}
+
+template<typename T>
+typename XLList<T>::Iterator XLList<T>::end() const {
+    return Iterator(tail, head);
+}
+
+template<typename T>
+bool XLList<T>::addElement(T newElement, int index) {
+    if (index < 0 || index > size) return false;
+    Node<T> *newNode = new Node<T>();
+    newNode->data = newElement;
+    if (size == 0) {
+        newNode->link = Iterator::xorNodes(head, tail);
+        head->link = newNode;
+        tail = newNode;
+    } else {
+        if (index == size) {
+            newNode->link = Iterator::xorNodes(tail, head);
+            tail->link = Iterator::xorNodes(tail->link, head);
+            tail->link = Iterator::xorNodes(tail->link, newNode);
+            tail = newNode;
+        } else {
+            Iterator listIterator = begin();
+            int currentIndex = 0;
+            while (currentIndex != index) {
+                ++listIterator;
+                ++currentIndex;
+            }
+            // Define link in new node.
+            newNode->link = Iterator::xorNodes(listIterator.nodePtrBefore, listIterator.nodePtrCurrent);
+            // Update link in prevoius node.
+            listIterator.nodePtrBefore->link = Iterator::xorNodes(listIterator.nodePtrBefore->link,
+                                                                  listIterator.nodePtrCurrent);
+            listIterator.nodePtrBefore->link = Iterator::xorNodes(listIterator.nodePtrBefore->link, newNode);
+            // Update link in current (next) node.
+            listIterator.nodePtrCurrent->link = Iterator::xorNodes(listIterator.nodePtrBefore,
+                                                                   listIterator.nodePtrCurrent->link);
+            listIterator.nodePtrCurrent->link = Iterator::xorNodes(newNode, listIterator.nodePtrCurrent->link);
+        }
+    }
+    ++size;
+    return true;
+}
+
+template<typename T>
+bool XLList<T>::removeElement(int index) {
+    if (index < 0 || index >= size) return false;
+    if (size == 1) {
+        tail = head;
+        delete head->link;
+    } else {
+        if (index == size - 1) {
+            Node<T> *previousNode = Iterator::xorNodes(tail->link, head);
+            previousNode->link = Iterator::xorNodes(previousNode->link, tail);
+            previousNode->link = Iterator::xorNodes(previousNode->link, head);
+            delete tail;
+            tail = previousNode;
+        } else {
+            Iterator listIterator = begin();
+            int currentIndex = 0;
+            while (currentIndex != index) {
+                ++listIterator;
+                ++currentIndex;
+            }
+            Node<T> *previousToDelete = listIterator.nodePtrBefore;
+            ++listIterator;
+            // Update link in prevoius node.
+            previousToDelete->link = Iterator::xorNodes(previousToDelete->link, listIterator.nodePtrBefore);
+            previousToDelete->link = Iterator::xorNodes(previousToDelete->link, listIterator.nodePtrCurrent);
+            // Update link in next node.
+            listIterator.nodePtrCurrent->link = Iterator::xorNodes(listIterator.nodePtrBefore,
+                                                                   listIterator.nodePtrCurrent->link);
+            listIterator.nodePtrCurrent->link = Iterator::xorNodes(previousToDelete, listIterator.nodePtrCurrent->link);
+
+            delete listIterator.nodePtrBefore;
+        }
+    }
+    --size;
+    return true;
+}
+
+template<typename T>
+T XLList<T>::getElement(int index) {
+    if (index < 0 || index >= size) return false;
+    Iterator listIterator = begin();
+    int currentIndex = 0;
+    while (currentIndex != index) {
+        ++listIterator;
+        ++currentIndex;
+    }
+    return  *listIterator;
+}
+
+template<typename T>
+bool XLList<T>::pushFront(T newElement){
+    return addElement(newElement, 0);
+}
+
+template<typename T>
+bool XLList<T>::pushBack(T newElement){
+    return addElement(newElement, size);
+}
+
+template<typename T>
+T XLList<T>::popFront(){
+    T element = getElement(0);
+    removeElement(0);
+    return element;
+}
+
+template<typename T>
+T XLList<T>::popBack(){
+    T element = getElement(size-1);
+    removeElement(size-1);
+    return element;
+}
+
+template<typename T>
+int XLList<T>::getSize() {
+    return size;
+}
 
 #endif //XLLIST_XLLIST_H
--- test/XLList_test.cpp
+++ test/XLList_test.cpp
@@ -4,11 +4,283 @@
 
 #include <gtest/gtest.h>
 #include "XLList.h"
+#include <stdlib.h>
+#include <time.h>
 
-TEST(iteratorTest, test1) {
+TEST(xLListTest, testXORFunction) {
     //arrange
+    Node<int> beforeNode;
+    Node<int> afterNode;
+    Node<int> *beforeNodePtr = &beforeNode;
+    Node<int> *afterNodePtr = &afterNode;
     //act
+    Node<int> *linkNode = XLList<int>::Iterator::xorNodes(beforeNodePtr, afterNodePtr);
+    Node<int> *linkNodeReverse = XLList<int>::Iterator::xorNodes(afterNodePtr, beforeNodePtr);
+    Node<int> *beforeNodeCalculated = XLList<int>::Iterator::xorNodes(afterNodePtr, linkNode);
+    Node<int> *afterNodeCalculated = XLList<int>::Iterator::xorNodes(linkNode, beforeNodePtr);
     //assert
-    bool trueVariable = true;
-    ASSERT_EQ(trueVariable, true);
+    ASSERT_EQ(linkNode,linkNodeReverse);
+    ASSERT_EQ(beforeNodePtr,beforeNodeCalculated);
+    ASSERT_EQ(afterNodePtr,afterNodeCalculated);
 }
+
+TEST(xLListTest, testObjectConstruction) {
+    //arrange
+    int afterCreationSize;
+    int afterCreationBytes;
+    int afterDeletionBytes;
+    //act
+    XLList<int> *list = new XLList<int>();
+    afterCreationSize = list->getSize();
+    afterCreationBytes = sizeof(*list);
+    XLList<int>::Iterator begin = list->begin();
+    XLList<int>::Iterator end = list->end();
+    delete list;
+    //assert
+    ASSERT_EQ(0,afterCreationSize);
+    ASSERT_EQ(24,afterCreationBytes);
+}
+
+TEST(xLListTest, testAddingOneElement) {
+    //arrange
+    int listSizeAfterAddingOneElement = 0;
+    //act
+    XLList<int> *list = new XLList<int>();
+    list->pushBack(1);
+    listSizeAfterAddingOneElement = list->getSize();
+    XLList<int>::Iterator begin = list->begin();
+    begin++;
+    XLList<int>::Iterator end = list->end();
+    delete list;
+    //assert
+    ASSERT_EQ(1,listSizeAfterAddingOneElement);
+    ASSERT_EQ(begin, end);
+}
+
+TEST(xLListTest, testAddingMultipleElementsAtTheEndOfTheList) {
+    //arrange
+    int listSizeAfterAddingMultipleElements = 0;
+    int numberOfElementsToAdd = 987;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    XLList<int>::Iterator begin = list->begin();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        begin++;
+    }
+    listSizeAfterAddingMultipleElements = list->getSize();
+    XLList<int>::Iterator end = list->end();
+    delete list;
+    //assert
+    ASSERT_EQ(numberOfElementsToAdd,listSizeAfterAddingMultipleElements);
+    ASSERT_EQ(begin, end);
+}
+
+TEST(xLListTest, testAddingMultipleElementsAtTheBeginingOfTheList) {
+    //arrange
+    int listSizeAfterAddingMultipleElements = 0;
+    int numberOfElementsToAdd = 987;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushFront(iter);
+    }
+    XLList<int>::Iterator begin = list->begin();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        begin++;
+    }
+    listSizeAfterAddingMultipleElements = list->getSize();
+    XLList<int>::Iterator end = list->end();
+    delete list;
+    //assert
+    ASSERT_EQ(numberOfElementsToAdd,listSizeAfterAddingMultipleElements);
+    ASSERT_EQ(begin, end);
+}
+TEST(xLListTest, testAddingMultipleElementsInTheMiddleOfTheList) {
+    //arrange
+    int listSizeAfterAddingMultipleElements = 0;
+    int numberOfElementsToAdd = 987;
+    srand (time(NULL));
+    //act
+    XLList<int> *list = new XLList<int>();
+    list->pushBack(-1); // The begining of the list
+    list->pushBack(-2); // The end of the list
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        int index = rand() % (list->getSize()-1) + 1;
+        list->addElement(iter, index);
+    }
+    XLList<int>::Iterator begin = list->begin();
+    for (int iter=0;iter<(numberOfElementsToAdd+2);++iter){
+        begin++;
+    }
+    listSizeAfterAddingMultipleElements = list->getSize();
+    XLList<int>::Iterator end = list->end();
+    delete list;
+    //assert
+    ASSERT_EQ(numberOfElementsToAdd+2,listSizeAfterAddingMultipleElements);
+    ASSERT_EQ(begin, end);
+}
+
+TEST(xLListTest, testRemovingElementsAtTheFrontOfTheList) {
+    //arrange
+    int listSizeAfterAddingAndRemovingMultipleElements = 0;
+    int numberOfElementsToAdd = 987;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    for (int iter=0;iter<(numberOfElementsToAdd-1);++iter){
+        list->popFront();
+    }
+    listSizeAfterAddingAndRemovingMultipleElements = list->getSize();
+    XLList<int>::Iterator begin = list->begin();
+    begin++;
+    XLList<int>::Iterator end = list->end();
+    delete list;
+    //assert
+    ASSERT_EQ(1,listSizeAfterAddingAndRemovingMultipleElements);
+    ASSERT_EQ(begin, end);
+}
+
+TEST(xLListTest, testRemovingMultipleElementsInTheMiddleOfTheList) {
+    //arrange
+    int listSizeAfterAddingAndRemovingMultipleElements = 0;
+    int numberOfElementsToAdd = 987;
+    srand (time(NULL));
+    //act
+    XLList<int> *list = new XLList<int>();
+    list->pushBack(-1); // The begining of the list
+    list->pushBack(-2); // The end of the list
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        int index = rand() % (list->getSize()-1) + 1;
+        list->addElement(iter, index);
+    }
+    for (int iter=0;iter<(numberOfElementsToAdd+1);++iter){
+        int index = rand() % (list->getSize()-1) + 1;
+        list->removeElement(index);
+    }
+    XLList<int>::Iterator begin = list->begin();
+    begin++;
+    XLList<int>::Iterator end = list->end();
+    listSizeAfterAddingAndRemovingMultipleElements = list->getSize();
+    delete list;
+    //assert
+    ASSERT_EQ(1,listSizeAfterAddingAndRemovingMultipleElements);
+    ASSERT_EQ(begin, end);
+}
+
+TEST(xLListTest, testRemovingElementsAtTheEndOfTheList) {
+    //arrange
+    int listSizeAfterAddingAndRemovingMultipleElements = 0;
+    int numberOfElementsToAdd = 987;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    for (int iter=0;iter<(numberOfElementsToAdd-1);++iter){
+        list->popBack();
+    }
+    listSizeAfterAddingAndRemovingMultipleElements = list->getSize();
+    XLList<int>::Iterator begin = list->begin();
+    begin++;
+    XLList<int>::Iterator end = list->end();
+    delete list;
+    //assert
+    ASSERT_EQ(1,listSizeAfterAddingAndRemovingMultipleElements);
+    ASSERT_EQ(begin, end);
+}
+
+TEST(xLListTest, testFetchingElementAtTheFrontOfTheList) {
+    //arrange
+    int numberOfElementsToAdd = 67;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    int frontElement = list->getElement(0);
+    delete list;
+    //assert
+    ASSERT_EQ(0, frontElement);
+}
+
+TEST(xLListTest, testFetchingElementAtTheEndtOfTheList) {
+    //arrange
+    int numberOfElementsToAdd = 67;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    int endElement = list->getElement(list->getSize()-1);
+    delete list;
+    //assert
+    ASSERT_EQ(66, endElement);
+}
+
+TEST(xLListTest, testFetchingElementInTheMiddleOfTheList) {
+    //arrange
+    int numberOfElementsToAdd = 67;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    int middleElement = list->getElement(numberOfElementsToAdd/2);
+    delete list;
+    //assert
+    ASSERT_EQ(numberOfElementsToAdd/2, middleElement);
+}
+
+TEST(xLListTest, testAddingOutOfBounds) {
+    //arrange
+    int numberOfElementsToAdd = 67;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    bool shouldBeFalseBecauseLessThanZero = list->addElement(13,-1);
+    bool shouldBeFalseBecauseGreaterThanSize = list->addElement(13,numberOfElementsToAdd+1);
+    delete list;
+    //assert
+    ASSERT_FALSE(shouldBeFalseBecauseLessThanZero);
+    ASSERT_FALSE(shouldBeFalseBecauseGreaterThanSize);
+}
+
+TEST(xLListTest, testRemovingOutOfBounds) {
+    //arrange
+    int numberOfElementsToAdd = 67;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    bool shouldBeFalseBecauseLessThanZero = list->removeElement(-1);
+    bool shouldBeFalseBecauseGreaterThanSize = list->removeElement(numberOfElementsToAdd+1);
+    delete list;
+    //assert
+    ASSERT_FALSE(shouldBeFalseBecauseLessThanZero);
+    ASSERT_FALSE(shouldBeFalseBecauseGreaterThanSize);
+}
+
+TEST(xLListTest, testFetchingOutOfBounds) {
+    //arrange
+    int numberOfElementsToAdd = 67;
+    //act
+    XLList<int> *list = new XLList<int>();
+    for (int iter=0;iter<numberOfElementsToAdd;++iter){
+        list->pushBack(iter);
+    }
+    bool shouldBeFalseBecauseLessThanZero = list->getElement(-1);
+    bool shouldBeFalseBecauseGreaterThanSize = list->getElement(numberOfElementsToAdd+1);
+    delete list;
+    //assert
+    ASSERT_FALSE(shouldBeFalseBecauseLessThanZero);
+    ASSERT_FALSE(shouldBeFalseBecauseGreaterThanSize);
+}
+
